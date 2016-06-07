017e5314273aff7bce24f104888866fa565cc3d7
Beetwig <beetwigonline@gmail.com>
2016-06-07 19:19:30 +0200

    import struct to the site (pages + controller + routes)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
+  def album
+  end
+  def contacts
+  end
+  def covoiturage
+  end
   def home
   end
-  def page
+  def hotel
+  end
+  def list
+  end
+  def place
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 
   get "/home" => "pages#home"
   get "/place" => "pages#place"
+  get "/hotel" => "pages#hotel"
+  get "/list" => "pages#list"
+  get "/album" => "pages#album"
+  get "/covoiturage" => "pages#covoiturage"
+  get "/contacts" => "pages#contacts"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
