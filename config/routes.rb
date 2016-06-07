be86d48f4c6cc350e5cc19e0caa9bb94f9929010
Beetwig <beetwigonline@gmail.com>
2016-06-07 08:48:28 +0200

    link home and place page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  get "/home" => "pages#home"
   get "/place" => "pages#place"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
